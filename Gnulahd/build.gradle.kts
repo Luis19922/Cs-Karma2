7689b06dcaa1a06ccaa0e38187288777e5f3b429
Luis19922 <128546309+Luis19922@users.noreply.github.com>
2026-02-28 19:45:26 -0500

    build.gradle.kts

--- Gnulahd/build.gradle.kts
+++ Gnulahd/build.gradle.kts
@@ -7,5 +7,5 @@ cloudstream {
     description = "GnulaHD"
     status  = 1
     tvTypes = listOf("Movie", "Anime", "TvSeries") //Movie, AnimeMovie, TvSeries, Cartoon, Anime, OVA, Torrent, Documentary, AsianDrama, Live, NSFW, Others, Music, AudioBook, CustomMedia, Audio, Podcast,
-    iconUrl = "https://t1.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=https://ww3.gnulahd.nu/&size=128"
+    iconUrl = "https://t1.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=https://ww2.tlnovelas.net//&size=128"
 }
